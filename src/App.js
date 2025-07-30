7b55abff9eb05cc6be33ce9b14649298fafe9b73
petrockc <petrockc@tecsysint.com>
2025-07-30 11:14:41 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProcessingStation from './components/ProcessingStation';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ProcessingStation />
+    </div>
+  );
+}
+
+export default App;
